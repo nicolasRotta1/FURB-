cb3b988f45a8b6b79c653b9630dc21ee7e3e4a33
Nícolas Rotta <nrotta@furb.br>
2026-02-18 21:24:27 -0300

    first commit

---
+++ Modulo1/src/Aula1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Aula1 {
+    public static void main(String[] args) {
+        System.out.println("BÂO");
+        System.out.println("Segunda linha");
+        System.out.println("Terceira item");
+        System.err.print("Quarto item");
+    }
+}
